82e33ee41337e31b830dd9517b9569af9c1a7d14
Ewerton Hecsley <ewerton.martinscomercial@gmail.com>
2025-09-17 22:29:43 -0300

    refactor: ajust in order of tecnologies

--- src/app/modules/portfolio/components/projects/projects.ts
+++ src/app/modules/portfolio/components/projects/projects.ts
@@ -9,44 +9,44 @@ import { Component, signal } from '@angular/core';
 export class Projects {
   public arrayProjects = signal([
     {
-      src:'assets/img/productService.jpeg',
-      alt:'Imagem api de servicos e produtos',
-      title:'API Product Service',
-      width:'100px',
-      height:'51px',
-      description:`<p>Microserviço para gestão de produtos, seguindo DDD e Clean Architecture, com logs estruturados e testes unitários.</p>`,
-      links:[
+      src: 'assets/img/productService.jpeg',
+      alt: 'Imagem api de servicos e produtos',
+      title: 'API Product Service',
+      width: '100px',
+      height: '51px',
+      description: `<p>Microserviço para gestão de produtos, seguindo DDD e Clean Architecture, com logs estruturados e testes unitários.</p>`,
+      links: [
         {
-          name:'Conheça o projeto',
-          href:'https://github.com/EwertonHecsley/node-projetc-sercive-rabbitmq'
+          name: 'Conheça o projeto',
+          href: 'https://github.com/EwertonHecsley/node-projetc-sercive-rabbitmq'
         }
       ]
     },
     {
-      src:'assets/img/financial.jpeg',
-      alt:'Imagem api de servico de transacoes financeiras',
-      title:'Financial Transactions',
-      width:'100px',
-      height:'51px',
-      description:`<p>API REST para controle de transações financeiras com autenticação JWT, categorização e extração de saldo.</p>`,
-      links:[
+      src: 'assets/img/financial.jpeg',
+      alt: 'Imagem api de servico de transacoes financeiras',
+      title: 'Financial Transactions',
+      width: '100px',
+      height: '51px',
+      description: `<p>API REST para controle de transações financeiras com autenticação JWT, categorização e extração de saldo.</p>`,
+      links: [
         {
-          name:'Conheça o projeto',
-          href:'https://github.com/EwertonHecsley/node-sistema-financial-transactions'
+          name: 'Conheça o projeto',
+          href: 'https://github.com/EwertonHecsley/node-sistema-financial-transactions'
         }
       ]
     },
     {
-      src:'assets/img/doctor.jpeg',
-      alt:'Imagem api de IA doctor',
-      title:'MedAssist',
-      width:'100px',
-      height:'51px',
-      description:`<p>API inteligente que transforma descrições clínicas simples em laudos técnicos usando IA generativa.</p>`,
-      links:[
+      src: 'assets/img/doctor.jpeg',
+      alt: 'Imagem api de IA doctor',
+      title: 'MedAssist',
+      width: '100px',
+      height: '51px',
+      description: `<p>API inteligente que transforma descrições clínicas simples em laudos técnicos usando IA generativa.</p>`,
+      links: [
         {
-          name:'Conheça o projeto',
-          href:'https://github.com/EwertonHecsley/node-api-medAssist'
+          name: 'Conheça o projeto',
+          href: 'https://github.com/EwertonHecsley/node-api-medAssist'
         }
       ]
     }
